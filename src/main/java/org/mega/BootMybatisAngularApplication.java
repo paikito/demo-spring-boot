8ff470678fc3cdeb13f92c912c3ffe08b73c13fe
paikito <epaikosv@gmail.com>
2018-02-23 14:57:30 -0500

    First commit

---
+++ src/main/java/org/mega/BootMybatisAngularApplication.java
@@ -0,0 +1,14 @@
+package org.mega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class BootMybatisAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMybatisAngularApplication.class, args);
+	}
+}
---
+++ src/test/java/org/mega/bootmybatisangular/BootMybatisAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mega.bootmybatisangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMybatisAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
