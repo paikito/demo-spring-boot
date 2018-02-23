b0419f19a484c81158136ab87bdf79ed5a008504
paikito <epaikosv@gmail.com>
2018-02-23 15:34:34 -0500

    Creacion de Entity

---
+++ src/main/java/org/mega/entity/User.java
@@ -0,0 +1,82 @@
+package org.mega.entity;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Access(AccessType.FIELD)
+public class User extends ParentEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "first-name", nullable = false)
+	private String firstName;
+
+	@Column(name = "second-name", nullable = false)
+	private String secondName;
+
+	@Column(name = "first_surname", nullable = false)
+	private String firstSurname;
+	
+	@Column(name="second_surname",nullable= false) 
+	private String secondSurname;
+	
+	@Column(name="phone",nullable= false) 
+	private String phone;
+	
+	@Column(name="address",nullable= false) 
+	private String address;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getFirstSurname() {
+		return firstSurname;
+	}
+
+	public void setFirstSurname(String firstSurname) {
+		this.firstSurname = firstSurname;
+	}
+
+	public String getSecondSurname() {
+		return secondSurname;
+	}
+
+	public void setSecondSurname(String secondSurname) {
+		this.secondSurname = secondSurname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
